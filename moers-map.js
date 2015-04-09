b923f7a4248cd00c2374fb969d80030fcebff11d
gruenzeug <juliane.wessalowski@gmail.com>
2015-04-09 23:49:47 +0200

    Popups und onClick events und zuviel auf einmal oO

--- moers-map.js
+++ moers-map.js
@@ -1,5 +1,5 @@
 
-var map = L.map('map').setView([51.4515, 6.6285], 15);
+var map = L.map('map').setView([51.4515, 6.6285], 12);
 
 //Der map einen Tile-Layer hinzufügen, damit man auch eine Karte sieht
 L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
@@ -7,8 +7,26 @@ L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
 	subdomains: ['otile1','otile2','otile3','otile4']
 }).addTo( map );
 
+function onEachFeature(feature, layer) {
+	//
+	if (feature.properties){
+		var popup = '';
+		if(feature.properties.name) { popup = popup + feature.properties.name;}
+		if(feature.properties.speed) { popup = popup + '<br>' + feature.properties.speed;}
+		if(feature.properties.date) { popup = popup + '<br>Start der Messung ' + feature.properties.date;}
+		if(feature.properties.pic) { popup = popup + '<br><img src="img/' + feature.properties.pic + '" width="200">';}
+		layer.bindPopup(popup);
+		layer.on('click', onClick);
+	}
+}
 
-//Mein erstes geojson Feature
+function onClick(e) {map.setZoomAround( this.getLatLng(), 14 );}
+//marker.on('click', function(e) {
+////    alert(e.latlng);
+//    map.setZoomAround( e.latlng, 14 )
+//});
+
+//Test geojson Feature
 var ackerstraße = {
 	"type": "Feature",
 	"properties":
@@ -25,21 +43,24 @@ var ackerstraße = {
 
 
 //Ein neues geojson Layer erstellen und der map hinzufügen
-var myLayer = L.geoJson().addTo(map);
+//var myLayer = L.geoJson().addTo(map);
 //Dem Layer ein neues Feature hinzufügen!
 //myLayer.addData(geojsonFeature);
-myLayer.addData(ackerstraße);
+//myLayer.addData(ackerstraße);
 
 //Alternativ ein einem:
 //L.geoJson(geojsonFeature).addTo(map);
+var layer1 = L.geoJson(messstellen, {
+			onEachFeature: onEachFeature
+		}).addTo(map);
 
 //Mehrere Marker usw zu einer Layer Group zusammenfassen
-var cities = L.layerGroup([marker, circle]);
+//var cities = L.layerGroup([marker, circle]);
 //Layer oder LayerGroups, die nachher als Overlay zur Auswahl stehen sollen
-var overlayMaps = {
-    "Cities": cities,
-    "geoJSON": myLayer
-};
+//var overlayMaps = {
+//    "Cities": cities,
+//    "geoJSON": myLayer
+//};
 //erstelle ein Layer Control, mit verschiedenen Base Layern und Overlays zur Auswahl :)
 //L.control.layers(baseMaps, overlayMaps).addTo(map);
-L.control.layers(null, overlayMaps).addTo(map);
+//L.control.layers(null, overlayMaps).addTo(map);
