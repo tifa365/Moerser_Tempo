ba99569e5af64e91fd9fd43ec65c3e34fde40918
stkdiretto <stefan@shutterworks.org>
2018-02-25 11:54:03 +0100

    fixed map tiles

--- moers-map.js
+++ moers-map.js
@@ -2,10 +2,8 @@
 var map = L.map('map').setView([51.4515, 6.6285], 12);
 
 //Der map einen Tile-Layer hinzufügen, damit man auch eine Karte sieht
-L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-	subdomains: ['otile1','otile2','otile3','otile4']
-}).addTo( map );
+L.tileLayer( 'http://tiles.codefor.de/static/bbs/germany/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors, Tiles: <a href="http://codefor.de">CfG-Map Server</a>'}).addTo( map );
 
 function onEachFeature(feature, layer) {
 	//
